03766352c1e1f515a4f51655cea81e06a0db805a
avandome <98584714+avandome@users.noreply.github.com>
2022-05-06 13:35:11 +0100

    save changes

--- alienGame.js
+++ alienGame.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,95 +12,19 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [{
-        question: "What is the name of the mummy that the Doctor encountered on the Orient Express?",
-        choice1: "The Foretold",
-        choice2: "The Gorgon",
-        choice3: "Tutankhamun",
-        choice4: "Hatshepsut",
-        answer: 1
-    },
-    {
-        question: "What planet do the Daleks originate from?",
-        choice1: "Telos",
-        choice2: "Earth",
-        choice3: "Skaro",
-        choice4: "Clom",
-        answer: 3
-    },
-    {
-        question: "What alien race is erased from your memory when you stop looking at them?",
-        choice1: "The Family of Blood",
-        choice2: "The Silence",
-        choice3: "The Daleks",
-        choice4: "The Sontarans",
-        answer: 2
-    },
-    {
-        question: "What species is the Doctor's friend Madame Vastra?",
-        choice1: "Sea Devil",
-        choice2: "Cyberman",
-        choice3: "Silurian",
-        choice4: "Saturnyn",
-        answer: 3
-    },
-    {
-        question: "What are the frightening stone angelic creatures known as?",
-        choice1: "Cupids",
-        choice2: "Weeping Angels",
-        choice3: "Hosts",
-        choice4: "Saviours",
-        answer: 2
-    },
-    {
-        question: "What is the approximate lifespan of the family of blood?",
-        choice1: "3 days",
-        choice2: "3 weeks",
-        choice3: "3 months",
-        choice4: "3 years",
-        answer: 3
-    },
-    {
-        question: "Which Dalek experimented on its own cells to introduce human elements?",
-        choice1: "Dalek Thay",
-        choice2: "Dalek Sec",
-        choice3: "Dalek Omega",
-        choice4: "Dalek Caan",
-        answer: 2
-    },
-    {
-        question: "What Earth creatures do the Judoon resemble?",
-        choice1: "Rhinoceros",
-        choice2: "Cats",
-        choice3: "Wasps",
-        choice4: "Wolf",
-        answer: 1
-    },
-    {
-        question: "Which of these aliens have NOT had at least one emotion removed?",
-        choice1: "Cyberman",
-        choice2: "Dalek",
-        choice3: "Modified Ood",
-        choice4: "Jagrafess",
-        answer: 4
-    },
-    {
-        question: "Who is the creator of the Daleks?",
-        choice1: "Davros",
-        choice2: "Kaled",
-        choice3: "Voord",
-        choice4: "Strax",
-        answer: 1
-    },
-    {
-        question: "What species was introduced in 1975 and re-appeared in the show's 50th anniversary special?",
-        choice1: "Slitheen",
-        choice2: "Jagrafess",
-        choice3: "Ood",
-        choice4: "Zygons",
-        answer: 3
-    }
-]
+let questions = []
+
+fetch("alienQuestions.JSON")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 //constants
 const CORRECT_BONUS = 10;
@@ -110,6 +36,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
@@ -124,7 +52,6 @@ getNewQuestion = () => {
     progressText.innerText = "Question " + questionCounter + "/" + MAX_QUESTIONS;
     //update progress bar
     progressBarFull.style.width = (questionCounter / MAX_QUESTIONS) * 100 + "%";
-    console.log(progressBarFull.style.width);
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex]
@@ -166,5 +93,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-
-startGame();
--- companionsGame.js
+++ companionsGame.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,128 +12,19 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [{
-        question: "What companion was known as the 'impossible girl?",
-        choice1: "Amy Pond",
-        choice2: "Clara Oswald",
-        choice3: "Rose Tyler",
-        choice4: "Donna Noble",
-        answer: 2
-    },
-    {
-        question: "What is River Song's cell number at the Stormcage 1 facility?",
-        choice1: "426",
-        choice2: "352",
-        choice3: "",
-        choice4: "",
-        answer: 1
-    },
-    {
-        question: "Which century was the second Doctor's commpanion Jamie McCrimmon from?",
-        choice1: "17th",
-        choice2: "18th",
-        choice3: "19th",
-        choice4: "21st",
-        answer: 2
-    },
-    {
-        question: "Which companion travelled with the Doctor for the most individual episodes?",
-        choice1: "Tegan",
-        choice2: "Jo",
-        choice3: "Sarah-Jane",
-        choice4: "Jamie",
-        answer: 4
-    },
-    {
-        question: "Who was the first companion to be 'expelled' from the TARDIS for bad behaviour?",
-        choice1: "Tegan",
-        choice2: "Adam",
-        choice3: "Zoe",
-        choice4: "Turlough",
-        answer: 2
-    },
-    {
-        question: "Which companion worked as an air hostess?",
-        choice1: "Adric",
-        choice2: "Ace",
-        choice3: "Tegan",
-        choice4: "Leela",
-        answer: 3
-    },
-    {
-        question: "What is the name of the council estate that Rose Tyler lived on?",
-        choice1: "Archer estate",
-        choice2: "Greene estate",
-        choice3: "Powell estate",
-        choice4: "Southwark estate",
-        answer: 3
-    },
-    {
-        question: "Which classic story features the first death of a companion in the show's history?",
-        choice1: "The Daleks' Masterplan",
-        choice2: "The Revenge of the Cybermen",
-        choice3: "Earthshock",
-        choice4: "The power of the Daleks",
-        answer: 1
-    },
-    {
-        question: "Which companion first joined the TARDIS crew as part of a plot to assassinate the Doctor?",
-        choice1: "Leela",
-        choice2: "Vislor Turlough",
-        choice3: "Captain Jack Harkness",
-        choice4: "Romana",
-        answer: 2
-    },
-    {
-        question: "How many siblings does Martha Jones have?",
-        choice1: "0",
-        choice2: "1",
-        choice3: "2",
-        choice4: "3",
-        answer: 3
-    },
-    {
-        question: "Which episode showed the first departure of a companion from the TARDIS",
-        choice1: "The Web of Fear",
-        choice2: "The Chase",
-        choice3: "The Daleks' Masterplan",
-        choice4: "The Dalek Invasion of Earth",
-        answer: 4
-    },
-    {
-        question: "Which companion was forced to have their memories of the Doctor erased forever?",
-        choice1: "Donna Noble",
-        choice2: "Claara Oswald",
-        choice3: "Amy Pond",
-        choice4: "Captain Jack Harkness",
-        answer: 2
-    },
-    {
-        question: "What was the first story to feature the doctor without a companion?",
-        choice1: "The Next Doctor",
-        choice2: "Heaven Sent",
-        choice3: "City of Death",
-        choice4: "The Deadly Assassin",
-        answer: 4
-    },
-    {
-        question: "What were the first companions to witness the Doctor regenerating?",
-        choice1: "Benn Jackson and Polly Wright",
-        choice2: "Jamie McCrimmon and Zoe Heriot",
-        choice3: "Ian Chesserton",
-        choice4: "Sarah-Jane Smith and Harry Sullivan",
-        answer: 1
-    },
-    {
-        question: "Which companion was converted into a Cyberman?",
-        choice1: "Clara Oswald",
-        choice2: "Martha Jones",
-        choice3: "Bill Potts",
-        choice4: "Rory Williams",
-        answer: 3
-    }
+let questions = []
 
-]
+fetch("companionsQuestions.JSON")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 //constants
 const CORRECT_BONUS = 10;
@@ -143,6 +36,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
@@ -157,7 +52,6 @@ getNewQuestion = () => {
     progressText.innerText = "Question " + questionCounter + "/" + MAX_QUESTIONS;
     //update progress bar
     progressBarFull.style.width = (questionCounter / MAX_QUESTIONS) * 100 + "%";
-    console.log(progressBarFull.style.width);
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex]
@@ -199,5 +93,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-
-startGame();
